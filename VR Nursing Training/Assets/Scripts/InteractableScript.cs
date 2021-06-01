d54b68bcd54d706c64d15a312e3da57994ac2f8b
Brennan Crowder <brennancrowder19@gmail.com>
2021-06-01 12:49:15 -0500

    Modify Scripts/Interactable Scene
    Added logic for assigning a "target" to a interactable
    Added second interactable in scene
    Added hitboxes to interactables
    Added XRRig to Interactable Scene

--- VR Nursing Training/Assets/Scripts/InteractableScript.cs
+++ VR Nursing Training/Assets/Scripts/InteractableScript.cs
@@ -6,11 +6,23 @@ public class InteractableScript : MonoBehaviour
 {
     public bool isInteractable;
     public Vector3 initialPosition;
+    public GameObject target;
 
     private void Start()
     {
         initialPosition = gameObject.transform.position;
         isInteractable = true;
+        
+    }
+
+    public void setTarget(GameObject inputTarget)
+    {
+        target = inputTarget;
+    }
+
+    public GameObject getTarget()
+    {
+        return target;
     }
 
     public void setInteractable(bool input)
