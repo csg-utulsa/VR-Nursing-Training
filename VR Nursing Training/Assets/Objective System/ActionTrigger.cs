69c79246aa8637101b9b27abd63e4da8fabd85d4
jhsharp <73131537+jhsharp@users.noreply.github.com>
2021-06-21 15:53:49 -0500

    ActionTrigger Script

---
+++ VR Nursing Training/Assets/Objective System/ActionTrigger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionTrigger : ActionBase
+{
+    public string[] targetTag;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        for (int i = 0; i < targetTag.Length; i++)
+        {
+            if (other.CompareTag(targetTag[i]))
+            {
+                performAction();
+            }
+        }
+    }
+}
