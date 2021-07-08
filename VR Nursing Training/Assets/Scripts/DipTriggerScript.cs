18861112aa0f2d3beb9a580c8820c055d246febe
Brennan Crowder <brennancrowder19@gmail.com>
2021-07-08 12:15:37 -0500

    Washing Hands
    Added Rigidbody to right hand
    Added Sphere Collider to right hand
    Added functionality to detect washing hands
    Added and modified some layers

--- VR Nursing Training/Assets/Scripts/DipTriggerScript.cs
+++ VR Nursing Training/Assets/Scripts/DipTriggerScript.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DipTriggerScript : MonoBehaviour
 {
     public Material material;
-
+    public UnityEvent handWash;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.GetComponent<InteractableScript>().isInteractable)
+        if (other.gameObject.CompareTag("Hands"))
+        {
+            Debug.Log("Washing Hands...");
+            handWash.Invoke();
+        }
+        else if (other.gameObject.GetComponent<InteractableScript>().isInteractable)
         {            
             other.gameObject.GetComponent<InteractableScript>().setMaterial(material);
         }
+
+        
     }
+
+
+
 }
