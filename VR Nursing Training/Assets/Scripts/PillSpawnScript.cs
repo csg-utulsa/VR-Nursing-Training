533523430cc68764e944b3e2439dd01bb3341f8d
Brennan Crowder <brennancrowder19@gmail.com>
2021-07-08 13:16:37 -0500

    Pill Type
    Added Pill Type String to pill bottles that transfers to respective pills and cut pills

--- VR Nursing Training/Assets/Scripts/PillSpawnScript.cs
+++ VR Nursing Training/Assets/Scripts/PillSpawnScript.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class PillSpawnScript : MonoBehaviour
 {
     public GameObject pill;
+    private GameObject pill1;
+    private GameObject pill2;
+    private GameObject pill3;
+
     private void OnTriggerEnter(Collider other)
     {
         
 
         if (other.CompareTag("PillContainer"))
         {
-            Instantiate(pill, transform.position + new Vector3(0, 1, 0), transform.rotation);
-            Instantiate(pill, transform.position + new Vector3(0, 1, 0), transform.rotation);
-            Instantiate(pill, transform.position + new Vector3(0, 1, 0), transform.rotation);
+            pill1 = Instantiate(pill, transform.position + new Vector3(0, 1, 0), transform.rotation);
+            pill2 = Instantiate(pill, transform.position + new Vector3(0, 1, 0), transform.rotation);
+            pill3 = Instantiate(pill, transform.position + new Vector3(0, 1, 0), transform.rotation);
+
+            pill1.GetComponent<InteractableScript>().setType(other.gameObject.GetComponent<InteractableScript>().getType());
+            pill2.GetComponent<InteractableScript>().setType(other.gameObject.GetComponent<InteractableScript>().getType());
+            pill3.GetComponent<InteractableScript>().setType(other.gameObject.GetComponent<InteractableScript>().getType());
         }
     }
 }
