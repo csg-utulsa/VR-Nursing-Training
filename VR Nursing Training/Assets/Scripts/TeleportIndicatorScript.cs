19eca08b9f218ad720252a9a3a4cf6c2ae3a101a
jhsharp <73131537+jhsharp@users.noreply.github.com>
2021-11-17 18:26:09 -0600

    Teleport Indicator Text Labels
    The teleport zones all have labels that can be edited from the inspector window.

--- VR Nursing Training/Assets/Scripts/TeleportIndicatorScript.cs
+++ VR Nursing Training/Assets/Scripts/TeleportIndicatorScript.cs
@@ -1,22 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TeleportIndicatorScript : MonoBehaviour
 {
     [SerializeField] private GameObject indicatorZone;
+    [SerializeField] private GameObject indicatorText;
+    [SerializeField] private string zoneName;
+    private TextMeshProUGUI text;
     private int cooldownMax = 3;
     private int cooldown = 0;
+    private Vector3 direction;
 
+    public void Awake()
+    {
+        text = indicatorText.GetComponent<TextMeshProUGUI>();
+        text.text = zoneName;
+    }
     public void Update()
     {
         if (cooldown > 0) cooldown--;
         else indicatorActive(false);
+        direction = (transform.position- Camera.main.transform.position).normalized;
+        direction.y = 0;
+        if (direction != Vector3.zero) indicatorText.gameObject.transform.rotation = Quaternion.LookRotation(direction);
     }
 
     public void indicatorActive(bool active)
     {
         if (active) cooldown = cooldownMax;
+        else cooldown = 0;
         indicatorZone.SetActive(active);
     }
 }
