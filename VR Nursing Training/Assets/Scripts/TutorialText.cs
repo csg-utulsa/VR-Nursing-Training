7b9e3049007d6bc092085c896e70c5cc29f3ad7c
Andres-galiasso <andres@galiasso.net>
2021-12-22 17:52:58 -0600

    SUMCHANGES

--- VR Nursing Training/Assets/Scripts/TutorialText.cs
+++ VR Nursing Training/Assets/Scripts/TutorialText.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshPro))]
 
 public class TutorialText : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string tutText;
+    private TextMeshPro dynamicText = null;
+
+    private void Awake()
     {
-        
+        dynamicText = gameObject.GetComponent<TextMeshPro>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (dynamicText != null)
+        {
+            dynamicText.SetText(tutText);
+        }
     }
 }
