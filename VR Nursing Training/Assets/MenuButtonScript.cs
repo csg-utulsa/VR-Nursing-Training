cefe3a26d1f9ea449328d462a36a1c52f0c7433d
Brennan Crowder <brennancrowder19@gmail.com>
2021-12-21 18:04:23 -0600

    added debug logs

--- VR Nursing Training/Assets/MenuButtonScript.cs
+++ VR Nursing Training/Assets/MenuButtonScript.cs
@@ -21,9 +21,11 @@ public class MenuButtonScript : InteractableBase
         if (flag == true)
         {
             MenuButtonManager.VRToggle = true;
+            Debug.Log("USING VR");
         } else
         {
             MenuButtonManager.VRToggle = false;
+            Debug.Log("NOT USING VR");
         }
         flag = false;
     }
