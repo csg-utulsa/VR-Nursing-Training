eac417b05705735ae0baa684a853a5db2f65a7d7
Rubicon7 <56270335+Rubicon7@users.noreply.github.com>
2021-12-26 17:01:06 -0600

    Code changes for the paper

--- VR Nursing Training/Assets/Scripts/Objective System/ActionCheckMedicine.cs
+++ VR Nursing Training/Assets/Scripts/Objective System/ActionCheckMedicine.cs
@@ -7,12 +7,14 @@ public class ActionCheckMedicine : ActionBase
     public bool notTarget = false; // If set to true, will activate if the pill's type is NOT in the targetType list
     public bool onCollision = false; // If set to true, will check any medicine it collides with
 
+
     private void OnTriggerEnter(Collider other)
     {
         if ((onCollision) && (other.CompareTag("Pill") || other.CompareTag("HalfPill") || other.CompareTag("Patch") || other.CompareTag("Liquid")))
         {
             Debug.Log("On Cart Trigger" + other.name);
             CheckMedicine(other.gameObject.GetComponent<InteractableScript>().getType());
+
         }
     }
 
--- VR Nursing Training/Assets/Scripts/PatchObjectScript.cs
+++ VR Nursing Training/Assets/Scripts/PatchObjectScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class PatchObjectScript : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class PatchObjectScript : MonoBehaviour
     private string initialText;
     private string[] arrayText;
     private string finalText;
+
+    public UnityEvent onDateSet;
+
 
     // Set text object when object is awake
     void Awake()
@@ -29,6 +33,7 @@ public class PatchObjectScript : MonoBehaviour
             arrayText = initialText.Split(' ');
             finalText = arrayText[0] + "\n" + arrayText[1];
             text.text = finalText;
+            onDateSet.Invoke();
         }
     }
 }
