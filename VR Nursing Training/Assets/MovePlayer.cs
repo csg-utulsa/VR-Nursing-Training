8ed3b9c6fa68f9cd8e57de4428e2c1805bb2e9da
jhsharp <73131537+jhsharp@users.noreply.github.com>
2022-06-20 18:57:14 -0500

    Modified MovePlayer to make it more modular

--- VR Nursing Training/Assets/MovePlayer.cs
+++ VR Nursing Training/Assets/MovePlayer.cs
@@ -6,21 +6,24 @@ using UnityEngine.Events;
 public class MovePlayer : InteractableBase
 {
     public GameObject player;
-
-    public UnityEvent whenPressed;
+    public GameObject moveLocation;
+    public UnityEvent moveActions;
 
     public void OnTriggerEnter(Collider other)
     {
-        Interact(other);
+        if (other.tag == "Hands")
+        {
+            Interact(other);
+        }
     }
 
     public override void Interact(Collider other)
     {
-        Debug.Log("test");
-        player.transform.position = new Vector3(21.1f, 5, 19f);
-        whenPressed.Invoke(); // invoke actions like transfer of medicine
+        player.transform.position = moveLocation.transform.position;
+        player.transform.rotation = moveLocation.transform.rotation;
+        moveActions.Invoke(); // invoke actions like transfer of medicine
     }
 
 }
 
-// TODO: Clean this script up to be more modular
+// TODO: Should be moved onto a proper button instead of using InteractableBase
