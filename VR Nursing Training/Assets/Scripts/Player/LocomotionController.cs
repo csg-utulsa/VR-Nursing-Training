0f5c919af1698734af0c83c5776e33d36e51fb97
BrennanCrowder <brennancrowder19@gmail.com>
2022-06-28 19:26:40 -0500

    XRRig Cleaning
    Removed old stuff Fixed new stuff

--- VR Nursing Training/Assets/Scripts/Player/LocomotionController.cs
+++ VR Nursing Training/Assets/Scripts/Player/LocomotionController.cs
@@ -12,56 +12,36 @@ public class LocomotionController : MonoBehaviour
     public XRController leftTeleportRay;
     public InputHelpers.Button teleportActivationButton;
     public float activationThreshhold = .1f;
-    public Canvas questionCanvas;
-    public Camera canvasCam;
     public bool toggleRightRayVisible;
     public bool toggleLeftRayVisible;
     // Update is called once per frame
     void Update()
     {
-        int count = 0;
-        /*foreach (Transform child in questionCanvas.transform)
-        {
-            if (child.gameObject.activeSelf)
-            {
-                count++;
-            }
-        }*/
-        
         if (rightTeleportRay)
         {
             if (toggleRightRayVisible)
             {
                 rightTeleportRay.gameObject.SetActive(true);
             }
-            else if (count > 0)
-            {
-                canvasCam.gameObject.SetActive(true);
-                rightTeleportRay.gameObject.SetActive(true);
-            }
             else
             {
-                canvasCam.gameObject.SetActive(false);
                 rightTeleportRay.gameObject.SetActive(CheckIfActivated(rightTeleportRay));
             }
-        }
+        } 
+        
         if (leftTeleportRay)
         {
             if (toggleLeftRayVisible)
             {
                 leftTeleportRay.gameObject.SetActive(true);
             }
-            else if (count > 0)
-            {
-                canvasCam.gameObject.SetActive(true);
-                leftTeleportRay.gameObject.SetActive(true);
-            }
             else
             {
-                canvasCam.gameObject.SetActive(false);
                 leftTeleportRay.gameObject.SetActive(CheckIfActivated(leftTeleportRay));
             }
-        }
+        } 
+       
+        
     }
 
     public bool CheckIfActivated(XRController controller)
