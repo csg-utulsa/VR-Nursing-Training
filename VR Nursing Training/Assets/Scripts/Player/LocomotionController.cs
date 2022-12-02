1484513b88bd0fdb590f07ea101e97e0d023cd15
Brennan Crowder <56270795+BrennanCrowder@users.noreply.github.com>
2022-12-01 19:24:39 -0600

    Added Ray Visual controls to XR Singleton
    Can enable/disable beam from singleton
    Added method that is called on scene load
    Deprecated old locomotion controller

--- VR Nursing Training/Assets/Scripts/Player/LocomotionController.cs
+++ VR Nursing Training/Assets/Scripts/Player/LocomotionController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
+    // DEPRECATED
+    
     // Allows for teleportation ray to only be cast when in use/activated by the "Primary2DAxis Up" input.
     // Video referenced = "Introduction to VR in Unity - PART 3 : TELEPORTATION" by Valem.
     
 public class LocomotionController : MonoBehaviour
 {
-    public XRController rightTeleportRay;
+    /*public XRController rightTeleportRay;
     public XRController leftTeleportRay;
     public InputHelpers.Button teleportActivationButton;
     public float activationThreshhold = .1f;
@@ -43,10 +45,10 @@ public class LocomotionController : MonoBehaviour
        
         
     }
-
+    
     public bool CheckIfActivated(XRController controller)
     {
         InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationThreshhold);
         return isActivated;
-    }
+    }*/
 }
--- VR Nursing Training/Assets/Scripts/XRRigSingleton.cs
+++ VR Nursing Training/Assets/Scripts/XRRigSingleton.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.XR.Management;
+using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class XRRigSingleton : MonoBehaviour
 {
     public static XRRigSingleton xrs { get; private set; }
+    
     /// <summary>
     /// Set true ONLY when a XR Rig is detected
     /// </summary>
@@ -19,15 +21,24 @@ public class XRRigSingleton : MonoBehaviour
     [SerializeField] private GameObject root3D;
     [Tooltip("3D Camera Reference")]
     [SerializeField] private Camera cam3D;
-    [Tooltip("RightController Reference")]
+    [Tooltip("Right Controller Reference")]
     [SerializeField] private GameObject rightController;
-    [Tooltip("LeftController Reference")]
+    [Tooltip("Right Teleport Ray Reference")]
+    [SerializeField] private GameObject rightTeleportRay;
+    [Tooltip("Left Controller Reference")]
     [SerializeField] private GameObject leftController;
+    [Tooltip("Left Teleport Ray Reference")]
+    [SerializeField] private GameObject leftTeleportRay;
+
     /// <summary>
     /// Set in setVRActive to either the 3D or VR camera
     /// </summary>
     private Camera activeCamera;
+
+    [Tooltip("Force into VR mode")]
     public bool debugVRActive = false;
+
+    [Tooltip("Force into 3D mode")]
     public bool debug3DActive = false;
 
     /// <summary>
@@ -35,37 +46,55 @@ public class XRRigSingleton : MonoBehaviour
     /// </summary>
     private void Awake()
     {
-        SceneManager.activeSceneChanged += NewScene;
         #region Singleton
         if (xrs == null)
         {
             xrs = this;
             DontDestroyOnLoad(this);
-        } else if (xrs != this)
+        } 
+        else if (xrs != this)
         {
             Debug.Log("DESTROYING DUPLICATE");
             xrs.transform.position = this.transform.position;
             Destroy(gameObject);
             return;
         }
+
         if ((XRGeneralSettings.Instance.Manager.activeLoader != null || debugVRActive) && !debug3DActive)
         {
             Debug.Log("XR Detected..");
-            setVRActive(true);
+            setVRActive(true,false);
         }
         else
         {
             Debug.Log("XR NOT Detected");
-            setVRActive(false);
+            setVRActive(false,false);
         }
         #endregion
+
+        // Set new scene listener
+        SceneManager.activeSceneChanged += NewScene;
+
+        // Set controller teleport hover events
+        var rightRay = rightTeleportRay.GetComponent<XRRayInteractor>();
+        rightRay.hoverEntered.AddListener(rightRayActivate);
+        rightRay.hoverExited.AddListener(rightRayDeactivate);
+
+        var leftRay = leftTeleportRay.GetComponent<XRRayInteractor>();
+        leftRay.hoverEntered.AddListener(leftRayActivate);
+        leftRay.hoverExited.AddListener(leftRayDeactivate);
     }
 
+    /// <summary>
+    /// Called when a new scene is loaded
+    /// </summary>
+    /// <param name="prev"></param>
+    /// <param name="next"></param>
     private void NewScene(Scene prev, Scene next)
     {
-        var a = GetComponent<LocomotionController>();
-        a.toggleRightRayVisible = false;
-        a.toggleLeftRayVisible = false;
+        // Deactivate Controller Rays
+        rightRayDeactivate();
+        leftRayDeactivate();
     }
 
 
@@ -109,21 +138,65 @@ public class XRRigSingleton : MonoBehaviour
     /// Sets public bool setVRActive to parameter and enables/disables cameras based on bool
     /// </summary>
     /// <param name="active"></param>
-    public bool setVRActive(bool active)
+    public bool setVRActive(bool active, bool resetScene = true)
     {
         if (active && XRGeneralSettings.Instance.Manager.activeLoader == null && !debugVRActive)
         {
             Debug.LogError("Attempted to enable VR, but failed to detect XR Device");
             return false;
-        } else
+        } 
+        else
         {
+            // Toggle Active Camera/Controls
             vrActive = active;
             rootVR.SetActive(vrActive);
             root3D.SetActive(!vrActive);
             activeCamera = active ? camVR : cam3D;
+
+            // Reset Scene
+            if (resetScene)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            
             return true;
         }
         
     }
 
+    /// <summary>
+    /// Listener for XR OnHoverEntered Event
+    /// </summary>
+    /// <param name="args"></param>
+    private void rightRayActivate(HoverEnterEventArgs args = null)
+    {
+        rightTeleportRay.GetComponent<XRInteractorLineVisual>().enabled = true;
+    }
+
+    /// <summary>
+    /// Listener for XR OnHoverExit Event
+    /// </summary>
+    /// <param name="args"></param>
+    private void rightRayDeactivate(HoverExitEventArgs args = null)
+    {
+        rightTeleportRay.GetComponent<XRInteractorLineVisual>().enabled = false;
+    }
+
+    /// <summary>
+    /// Listener for XR OnHoverEntered Event
+    /// </summary>
+    /// <param name="args"></param>
+    private void leftRayActivate(HoverEnterEventArgs args = null)
+    {
+        leftTeleportRay.GetComponent<XRInteractorLineVisual>().enabled = true;
+    }
+
+    /// <summary>
+    /// Listener for XR OnHoverExit Event
+    /// </summary>
+    /// <param name="args"></param>
+    private void leftRayDeactivate(HoverExitEventArgs args = null)
+    {
+        leftTeleportRay.GetComponent<XRInteractorLineVisual>().enabled = false;
+    }
 }
