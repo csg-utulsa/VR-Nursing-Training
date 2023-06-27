8c268cc50144b8635267700a913a97d3d3242fe1
BrennanCrowder <brennancrowder19@gmail.com>
2023-06-27 01:52:40 -0500

    Beefy Changes
    Updated XR Rig V2 prefab
    Heavily modified Singleton script
    Modified inputs so mouse controller works
    NOTE: I have not pushed the removal of the old input script so this may break on your systems while I figure out what to change

--- VR Nursing Training/Assets/Scripts/XRRigSingleton.cs
+++ VR Nursing Training/Assets/Scripts/XRRigSingleton.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine.XR.Management;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 
+[RequireComponent(typeof(PlayerInput))]
 public class XRRigSingleton : MonoBehaviour
 {
     public static XRRigSingleton xrs { get; private set; }
@@ -13,33 +16,43 @@ public class XRRigSingleton : MonoBehaviour
     /// Set true ONLY when a XR Rig is detected
     /// </summary>
     private bool vrActive;
-    [Tooltip("VR Root Reference")]
-    [SerializeField] private GameObject rootVR;
-    [Tooltip("VR Camera Reference")]
-    [SerializeField] private Camera camVR;
-    [Tooltip("3D Root Reference")]
-    [SerializeField] private GameObject root3D;
-    [Tooltip("3D Camera Reference")]
-    [SerializeField] private Camera cam3D;
+    //[Tooltip("VR Root Reference")]
+    //[SerializeField] private GameObject rootVR;
+    [Tooltip("Camera Reference")]
+    [SerializeField] private Camera cam;
+    //[Tooltip("VR Camera Reference")]
+    //[SerializeField] private Camera camVR;
+    //[Tooltip("3D Root Reference")]
+    //[SerializeField] private GameObject root3D;
+    //[Tooltip("3D Camera Reference")]
+    //[SerializeField] private Camera cam3D;
     [Tooltip("Right Controller Reference")]
     [SerializeField] private GameObject rightController;
-    [Tooltip("Right Teleport Ray Reference")]
-    [SerializeField] private GameObject rightTeleportRay;
+    //[Tooltip("Right Teleport Ray Reference")]
+    //[SerializeField] private GameObject rightTeleportRay;
     [Tooltip("Left Controller Reference")]
     [SerializeField] private GameObject leftController;
-    [Tooltip("Left Teleport Ray Reference")]
-    [SerializeField] private GameObject leftTeleportRay;
 
-    /// <summary>
-    /// Set in setVRActive to either the 3D or VR camera
-    /// </summary>
-    private Camera activeCamera;
+    [Tooltip("Mouse Controller Reference")]
+    [SerializeField] private GameObject mouseController;
+    //[Tooltip("Left Teleport Ray Reference")]
+    //[SerializeField] private GameObject leftTeleportRay;
+
+    
+    private PlayerInput _playerInput;
+
+    private InputAction _mouseLookAction;
+
+    private InputAction _keyboardMoveAction;
 
     [Tooltip("Force into VR mode")]
     public bool debugVRActive = false;
 
     [Tooltip("Force into 3D mode")]
     public bool debug3DActive = false;
+
+    private Rigidbody _playerRB;
+   
 
     /// <summary>
     /// Creates singleton/destroys if already exists, Sets starting camera
@@ -63,12 +76,12 @@ public class XRRigSingleton : MonoBehaviour
         if ((XRGeneralSettings.Instance.Manager.activeLoader != null || debugVRActive) && !debug3DActive)
         {
             Debug.Log("XR Detected..");
-            setVRActive(true,false);
+            //setVRActive(true,false);
         }
         else
         {
             Debug.Log("XR NOT Detected");
-            setVRActive(false,false);
+            //setVRActive(false,false);
         }
         #endregion
 
@@ -76,13 +89,39 @@ public class XRRigSingleton : MonoBehaviour
         SceneManager.activeSceneChanged += NewScene;
 
         // Set controller teleport hover events
-        var rightRay = rightTeleportRay.GetComponent<XRRayInteractor>();
+        //var rightRay = rightTeleportRay.GetComponent<XRRayInteractor>();
+        var rightRay = rightController.GetComponent<XRRayInteractor>();
         rightRay.hoverEntered.AddListener(rightRayActivate);
         rightRay.hoverExited.AddListener(rightRayDeactivate);
 
-        var leftRay = leftTeleportRay.GetComponent<XRRayInteractor>();
+        //var leftRay = leftTeleportRay.GetComponent<XRRayInteractor>();
+        var leftRay = leftController.GetComponent<XRRayInteractor>();
         leftRay.hoverEntered.AddListener(leftRayActivate);
         leftRay.hoverExited.AddListener(leftRayDeactivate);
+
+        var mouseRay = mouseController.GetComponent<XRRayInteractor>();
+        mouseRay.hoverEntered.AddListener(mouseRayActivate);
+        mouseRay.hoverExited.AddListener(mouseRayDeactivate);
+
+        _playerRB = GetComponent<Rigidbody>();
+
+        _playerInput = GetComponent<PlayerInput>();
+        foreach(InputAction action in _playerInput.actions)
+        {
+            action.Enable();
+        }
+        _playerInput.onControlsChanged += OnControlsChanged;
+        _mouseLookAction = _playerInput.actions.FindAction("MouseLook");
+        //_keyboardMoveAction = _playerInput.actions.FindAction("KeyboardMovement");
+     
+    }
+
+    private void OnDisable()
+    {
+        if (xrs == this)
+        {
+            _playerInput.actions = null;
+        }
     }
 
     /// <summary>
@@ -97,23 +136,20 @@ public class XRRigSingleton : MonoBehaviour
         leftRayDeactivate();
     }
 
-
-    /// <summary>
-    /// Returns the VR master gameobject for the VR Camera
-    /// </summary>
-    /// <returns></returns>
-    public GameObject getRootVR() // May not need
+    private void OnControlsChanged(PlayerInput input)
     {
-        return rootVR;
-    }
-
-    /// <summary>
-    /// Returns the 3D master gameobject for the 3D camera
-    /// </summary>
-    /// <returns></returns>
-    public GameObject getRoot3D() // May not need
-    {
-        return root3D;
+        Debug.Log("Controls changed to: " + input.currentControlScheme);
+        Cursor.lockState = CursorLockMode.None;
+        if (input.currentControlScheme == "XRFullBody" || debugVRActive)
+        {
+            
+            setVRActive(true, false);
+        } 
+        else if (input.currentControlScheme == "Mouse&Keyboard" || debug3DActive)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            setVRActive(false, false);
+        }
     }
 
     /// <summary>
@@ -122,7 +158,8 @@ public class XRRigSingleton : MonoBehaviour
     /// <returns></returns>
     public Camera getActiveCamera()
     {
-        return activeCamera;
+        //return activeCamera;
+        return cam;
     }
 
     /// <summary>
@@ -149,9 +186,12 @@ public class XRRigSingleton : MonoBehaviour
         {
             // Toggle Active Camera/Controls
             vrActive = active;
-            rootVR.SetActive(vrActive);
-            root3D.SetActive(!vrActive);
-            activeCamera = active ? camVR : cam3D;
+
+            SetVrInputsActive(vrActive);
+            
+            //rootVR.SetActive(vrActive);
+            //root3D.SetActive(!vrActive);
+            //activeCamera = active ? camVR : cam3D;
 
             // Reset Scene
             if (resetScene)
@@ -164,13 +204,63 @@ public class XRRigSingleton : MonoBehaviour
         
     }
 
+    private bool SetVrInputsActive(bool active)
+    {
+        if (active)
+        {
+            Debug.Log("Disabling Mouse Controls...");
+            DisableKeyboardInputs();
+            Debug.Log("Enabling VR Controls...");
+            EnableVRInputs();
+        } 
+        else
+        {
+            Debug.Log("Disabling VR Controls...");
+            DisableVRInputs();
+            Debug.Log("Enabling Mouse Controls...");
+            EnableKeyboardInputs();
+        }
+
+        return true;
+    }
+
+    private bool EnableKeyboardInputs()
+    {
+        cam.GetComponent<TrackedPoseDriver>().enabled = false;
+        _mouseLookAction.performed += MouseLook;
+        //_keyboardMoveAction.performed += KeyboardMovement;
+        mouseController.SetActive(true);
+        return true;
+    }
+    private bool DisableKeyboardInputs()
+    {
+        cam.GetComponent<TrackedPoseDriver>().enabled = true;
+        _mouseLookAction.performed -= MouseLook;
+        //_keyboardMoveAction.performed -= KeyboardMovement;
+        mouseController.SetActive(false);
+        return true;
+    }
+    private bool EnableVRInputs()
+    {
+        leftController.SetActive(true);
+        rightController.SetActive(true);
+        return true;
+    }
+    private bool DisableVRInputs()
+    {
+        leftController.SetActive(false);
+        rightController.SetActive(false);
+        return true;
+    }
+
     /// <summary>
     /// Listener for XR OnHoverEntered Event
     /// </summary>
     /// <param name="args"></param>
     private void rightRayActivate(HoverEnterEventArgs args = null)
     {
-        rightTeleportRay.GetComponent<XRInteractorLineVisual>().enabled = true;
+        /*rightTeleportRay.GetComponent<XRInteractorLineVisual>().enabled = true;*/
+        rightController.GetComponent<XRInteractorLineVisual>().enabled = true;
     }
 
     /// <summary>
@@ -179,7 +269,8 @@ public class XRRigSingleton : MonoBehaviour
     /// <param name="args"></param>
     private void rightRayDeactivate(HoverExitEventArgs args = null)
     {
-        rightTeleportRay.GetComponent<XRInteractorLineVisual>().enabled = false;
+        /*rightTeleportRay.GetComponent<XRInteractorLineVisual>().enabled = false;*/
+        rightController.GetComponent<XRInteractorLineVisual>().enabled = false;
     }
 
     /// <summary>
@@ -188,7 +279,8 @@ public class XRRigSingleton : MonoBehaviour
     /// <param name="args"></param>
     private void leftRayActivate(HoverEnterEventArgs args = null)
     {
-        leftTeleportRay.GetComponent<XRInteractorLineVisual>().enabled = true;
+        /*leftTeleportRay.GetComponent<XRInteractorLineVisual>().enabled = true;*/
+        leftController.GetComponent<XRInteractorLineVisual>().enabled = true;
     }
 
     /// <summary>
@@ -197,6 +289,94 @@ public class XRRigSingleton : MonoBehaviour
     /// <param name="args"></param>
     private void leftRayDeactivate(HoverExitEventArgs args = null)
     {
-        leftTeleportRay.GetComponent<XRInteractorLineVisual>().enabled = false;
+        /*leftTeleportRay.GetComponent<XRInteractorLineVisual>().enabled = false;*/
+        leftController.GetComponent<XRInteractorLineVisual>().enabled = false;
+    }
+
+    /// <summary>
+    /// Listener for XR OnHoverEntered Event
+    /// </summary>
+    /// <param name="args"></param>
+    private void mouseRayActivate(HoverEnterEventArgs args = null)
+    {
+        //mouseController.GetComponent<XRInteractorLineVisual>().enabled = true;
+    }
+
+    /// <summary>
+    /// Listener for XR OnHoverExit Event
+    /// </summary>
+    /// <param name="args"></param>
+    private void mouseRayDeactivate(HoverExitEventArgs args = null)
+    {
+        //mouseController.GetComponent<XRInteractorLineVisual>().enabled = false;
+    }
+
+    /// <summary>
+    /// Tracks left and right rotation from mouse
+    /// </summary>
+    private float _deltaYRotation = 0f;
+
+    /// <summary>
+    /// Tracks up and down rotation from mouse
+    /// </summary>
+    private float _deltaXRotation = 0f;
+    [Tooltip("Mouse sensitivity"), SerializeField]
+    private float _mouseSensitivity = .1f;
+   
+
+    /// <summary>
+    /// Controls camera movement based on mouse
+    /// </summary>
+    /// <param name="args"></param>
+    private void MouseLook(InputAction.CallbackContext args)
+    {
+       
+        if (vrActive)
+        {
+            return;
+        }
+        Vector2 mouseLook = args.ReadValue<Vector2>();
+        _deltaYRotation += mouseLook.x * _mouseSensitivity;
+        _deltaXRotation -= mouseLook.y * _mouseSensitivity;
+        _deltaXRotation = Mathf.Clamp(_deltaXRotation, -90, 90);
+        cam.transform.rotation = Quaternion.Euler(_deltaXRotation, _deltaYRotation, 0f);
+    }
+    /*
+    Vector3 moveDir = Vector3.zero;
+    Vector2 movement = Vector2.zero;
+    private void KeyboardMovement(InputAction.CallbackContext args)
+    {
+        Debug.Log("Moving:");
+        movement = args.ReadValue<Vector2>();
+       
+        
+    }
+    
+    private void FixedUpdate()
+    {
+        //Vector3 camT = new Vector3(0, cam.transform.rotation.y, cam.transform.rotation.z)
+        moveDir = cam.transform.forward * movement.y + cam.transform.right * movement.x;
+        moveDir.y = 0;
+        _playerRB.AddForce(moveDir.normalized * 10f, ForceMode.Force);
+    }*/
+    
+    public void test()
+    {
+        Debug.Log("TEST");
+    }
+
+    public void hover()
+    {
+        Debug.Log("Hover");
+    }
+
+    public void select()
+    {
+        Debug.Log("Select");
+    }
+
+    public void activate()
+    {
+        Debug.Log("Activate");
     }
 }
