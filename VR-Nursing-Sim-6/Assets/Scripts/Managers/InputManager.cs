2df33d55167035e24d1505a1e4dff8a16122f49e
Zerothedragon <morgandigimon@gmail.com>
2025-09-19 15:15:32 -0500

    Swapped and Added Scripts

--- VR-Nursing-Sim-6/Assets/Scripts/Managers/InputManager.cs
+++ VR-Nursing-Sim-6/Assets/Scripts/Managers/InputManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UTulsa.VRNS.BaseScripts;
 using UnityEngine.InputSystem;
 using NUnit.Framework.Constraints;
-
+//// Looks as if this script may be unnecesarry, but will be kept just in case
 namespace UTulsa.VRNS.Managers
 {
     public class InputManager : Singleton<InputManager> //The basic idea behind this script is to manage all of the player's inputs, regardless of input method
@@ -17,7 +17,7 @@ namespace UTulsa.VRNS.Managers
             if (true)
             {
                 //This will be for Desktop mode, which we have to use right now
-                playerInput.Enable(); //Later we need to make sure it only activates the correct ActionMap based on input device
+                //playerInput.Enable(); //Later we need to make sure it only activates the correct ActionMap based on input device
             }
             else
             {
